d5c647096d8b1c7dbc0ea043a846135c8e810848
trungbaoduong <trungbao@itviec.com>
2023-11-14 10:54:17 +0700

    c3: resilience

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "source" ]
+  static classes = [ "supported" ]
+
+  connect() {
+    if ("clipboard" in navigator) {
+      this.element.classList.add(this.supportedClass);
+    }
+  }
 
   copy(event) {
     // to prevent browser trigger event
